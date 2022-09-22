c8668e8e2bce0f13090da6ab9a1eeded5e867ba5
Roman-Ojha <razzroman98@gmail.com>
2022-09-22 23:42:45 +0545

    feat(tabs_vuex): create Mutation & Store types & tabs state

---
+++ src/store/modules/tabs/state.ts
@@ -0,0 +1,13 @@
+import { TabsState } from "./types";
+
+const tabState: TabsState = {
+  activeTab: 1,
+  numberOfTabs: 2,
+  tabs: [
+    {
+      name: "R-CLI 1",
+    },
+  ],
+};
+
+export default tabState;
--- src/store/modules/tabs/types.ts
+++ src/store/modules/tabs/types.ts
@@ -1,4 +1,10 @@
 import { RootState } from "../../types";
+import {
+  ActionContext,
+  Store as VuexStore,
+  CommitOptions,
+  DispatchOptions,
+} from "vuex";
 
 export interface Tab {
   name: string;
@@ -39,4 +45,35 @@ type AugmentedActionContext = {
     key: K,
     payload: Parameters<Mutations[K]>[1]
   ): ReturnType<Mutations[K]>;
-} & Omit<ActionContext<State, RootState>, "commit">;
+} & Omit<ActionContext<TabsState, RootState>, "commit">;
+
+export enum TabsMutationType {
+  CHANGE_TAB = "CHANGE_TAB",
+  OPEN_NEW_TAB = "OPEN_NEW_TAB",
+}
+
+export type Mutations<S = TabsState> = {
+  [TabsMutationType.CHANGE_TAB](state: S, tabNo: number): void;
+  [TabsMutationType.OPEN_NEW_TAB](state: S): void;
+};
+
+export type TabsStore<S = TabsState> = Omit<
+  VuexStore<S>,
+  "getters" | "commit" | "dispatch"
+> & {
+  commit<K extends keyof Mutations, P extends Parameters<Mutations[K]>[1]>(
+    key: K,
+    payload: P,
+    options?: CommitOptions
+  ): ReturnType<Mutations[K]>;
+} & {
+  dispatch<K extends keyof Actions>(
+    key: K,
+    payload: Parameters<Actions[K]>[1],
+    options?: DispatchOptions
+  ): ReturnType<Actions[K]>;
+} & {
+  getters: {
+    [K in keyof Getters]: ReturnType<Getters[K]>;
+  };
+};
